fcfe3bd810ecc03915c23d24dabf8b35ede78d8d
nirzaf <mfmfazrin1986@gmail.com>
2022-01-28 09:07:49 +0530

    Add project files.

---
+++ DynamicBinding/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace DynamicBinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            dynamic d = i;
+            long l = d;
+        }
+    }
+}
